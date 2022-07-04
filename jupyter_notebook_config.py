4de1a87d01407f26b928b57e260fb807ff34cd9a
ruidong.wang <ruidong.wang@biomind.ai>
2022-07-04 16:52:30 +0800

    chapter6 patch1

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c.NotebookApp.allow_root = True
+#c.NotebookApp.notebook_dir = '/notebooks'
+c.NotebookApp.password = 'sha1:ba491c2759df:141e3416de4b8fb2b2f93d95a6c8cacfdae8b26c'
+c.NotebookApp.open_browser = False
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8888
+c.NotebookApp.allow_origin="*"
